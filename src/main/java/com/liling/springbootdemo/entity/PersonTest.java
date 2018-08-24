091e7bee513e1ed5e4d76a98da8401a8a3fc2c2b
liling <liling1@asp.citic.com>
2018-08-24 22:51:03 +0800

    add:new branch
    瞎写的

--- src/main/java/com/liling/springbootdemo/entity/PersonTest.java
+++ src/main/java/com/liling/springbootdemo/entity/PersonTest.java
@@ -21,10 +21,6 @@ public class PersonTest implements Serializable{
     public PersonTest() {
     }
 
-    public int getId() {
-        return id;
-    }
-
     public void setId(int id) {
         this.id = id;
     }

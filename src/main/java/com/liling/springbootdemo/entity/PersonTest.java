d3be4a9ec3a1349874318919cc0e3e62536f3f54
liling <liling1@asp.citic.com>
2018-08-25 11:46:57 +0800

    测试

--- src/main/java/com/liling/springbootdemo/entity/PersonTest.java
+++ src/main/java/com/liling/springbootdemo/entity/PersonTest.java
@@ -21,9 +21,6 @@ public class PersonTest implements Serializable{
     public PersonTest() {
     }
 
-    public int getId() {
-        return id;
-    }
 
     public void setId(int id) {
         this.id = id;
